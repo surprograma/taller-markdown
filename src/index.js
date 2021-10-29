81f04bd9b83dbb7563462ab7dc039fa1646602d3
Federico Aloi <federico.aloi@gmail.com>
2021-10-29 15:59:27 -0300

    Ultimos toques

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Reveal from 'reveal.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 import Highlight from 'reveal.js/plugin/highlight/highlight.esm.js';
+import Math from 'reveal.js/plugin/math/math.esm.js';
 import Zoom from 'reveal.js/plugin/zoom/zoom.esm.js';
 import PlantUml from './plantUml';
 
@@ -10,7 +11,13 @@ import 'reveal.js/plugin/highlight/zenburn.css';
 
 const slide = new Reveal({
   hash: true,
-  plugins: [Markdown, Highlight, Zoom, PlantUml],
+  math: {
+    mathjax: 'https://cdn.jsdelivr.net/gh/mathjax/mathjax@2.7.8/MathJax.js',
+    config: 'TeX-AMS_HTML-full',
+    // pass other options into `MathJax.Hub.Config()`
+    TeX: { Macros: { RR: '{\\bf R}' } },
+  },
+  plugins: [Markdown, Highlight, Zoom, PlantUml, Math],
 });
 
 slide.initialize();
